1923ce6f17cdca08c45d8987a2177f6c7bfd18ef
Htut-Aung-Wai <htutaungwai333@gmail.com>
2025-01-22 15:48:48 +0630

    ComponentScanCustomScan_commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.demo") //custom package component scan
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(DemoApplication.class, args); //delete al and just use this to run all
+		//SpringApplication.run(DemoApplication.class, args); //delete all and just use this to run all
 
 
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
